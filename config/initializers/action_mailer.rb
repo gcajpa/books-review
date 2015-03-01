40d2d5aaea6a75f195bdb467f99fb6bcf4c13341
Gustavo Corrêa Alves <gcajpa@gmail.com>
2015-03-01 19:22:30 -0300

    sendgrid setup

--- config/initializers/action_mailer.rb
+++ config/initializers/action_mailer.rb
@@ -9,3 +9,15 @@ Rails.env.on(:test, :production) do |config|
     host: 'http://www.booklist.com'
   }
 end
+Rails.env.on(:test, :production) do |config|
+  config.action_mailer.delivery_method = :smtp
+  ActionMailer::Base.smtp_settings = {
+    address: 'smtp.sendgrid.net',
+    port: '587',
+    authentication: :plain,
+    user_name: ENV['SENDGRID_USERNAME'],
+    password: ENV['SENDGRID_PASSWORD'],
+    domain: ENV['DOMAIN'],
+    enable_starttls_auto: true
+  }
+end
